dd348eae2cd4439d502897141dd9d338ed1b8d7c
kamishiro <xkamishiro624@gmail.com>
2026-01-14 16:10:16 -0500

    Fixed int variable truncating double on delay declaration

--- seqTextReader.java
+++ seqTextReader.java
@@ -10,7 +10,7 @@ public class seqTextReader
     
     public static void main(String[] args)
     {
-        int delay = 0.035 // 0.035 is default delay factor (seconds). line 44 used
+        double delay = 0.035 // 0.035 is default delay factor (seconds). line 44 used
         ArrayList<String> streamText = new ArrayList<String>();
         
         Scanner input = new Scanner(System.in);
